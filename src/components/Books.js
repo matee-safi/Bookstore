fe14403f0595990c8834c82687aed9d265263807
matee-safi <matiullahsafibbc@gmail.com>
2023-04-12 14:22:39 +0430

    Display books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,20 @@
-const books = () => {
-  const books = [];
+import { useSelector } from 'react-redux';
+// import { addBook } from '../redux/books/booksSlice';
 
+const Books = () => {
+  const books = useSelector((state) => state.books.books);
+  // const dispatch = useDispatch();
+  // const handleBookSubmit = (e) => {
+  //   e.preventDefault();
+  //   const title = e.target.previousElementSibling.previousElementSibling.value;
+  //   const category = e.target.previousElementSibling.value;
+  //   dispatch(addBook({ title, category }));
+  // };
   return (
     <>
       <div className="book-list">
         { books && books.map((book) => (
-          <p key={book.id}>
+          <p key={book.item_id}>
             {book.title}
             {' '}
             written by
@@ -18,8 +27,9 @@ const books = () => {
       <div className="add-book">
         <h2>Add new book</h2>
         <form>
-          <input type="text" placeholder="Book title" />
-          <select name="category" id="category">
+          <input type="text" placeholder="Book title" required />
+          <select name="category" id="category" defaultValue="">
+            <option value="" disabled>Category</option>
             <option value="Action">Action</option>
             <option value="Science fiction">Science fiction</option>
             <option value="Economy">Economy</option>
@@ -31,4 +41,4 @@ const books = () => {
   );
 };
 
-export default books;
+export default Books;
